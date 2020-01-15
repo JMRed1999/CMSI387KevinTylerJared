98e9a2f4b75d11095ee687b0b7ffe572e2fb86f0
JMRed1999 <jrebuyon@lion.lmu.edu>
2020-01-15 11:40:14 -0800

    Made folder hierarchy; First classwork added

---
+++ Classwork/Classwork01/SayHello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LIMIT 100
+
+int main ( int argc, char * argv[] ) {
+  char name[LIMIT];
+
+  printf("Enter your name: ");
+  gets(name);
+  printf("Hello, %s", name);
+
+  return 0;
+}
