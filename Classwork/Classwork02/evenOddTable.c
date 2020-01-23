db417b5a02a1aa639f1ec543e4cbab21ef833675
Tyler Ilunga <tylerilunga1@gmail.com>
2020-01-22 18:41:41 -0800

    classwork02

---
+++ Classwork/Classwork02/evenOddTable.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "evenOddTable.h"
+
+int main()
+{
+    int odds = 0;
+    int evens = 0;
+    for (int i = MIN_INT_VALUE; i <= MAX_INT_VALUE; i++)
+    {
+        print_output_line(i, i * i, compute_parity(i, &odds, &evens));
+    }
+    printf("#odd numbers = %d\n#even numbers = %d\n", odds, evens);
+    return 0;
+}
+
+int compute_parity(int value, int *odds, int *evens)
+{
+    if (value % 2 == 0)
+    {
+        *evens += 1;
+        return 0;
+    }
+    *odds += 1;
+    return 1;
+}
+
+void print_output_line(int value, int squaredValue, int parity)
+{
+    char *parity_string = ODD_PARITY;
+    if (parity == 0)
+    {
+        parity_string = EVEN_PARITY;
+    }
+    printf("%d\t%d\t%s\n", value, squaredValue, parity_string);
+}
---
+++ Classwork/Classwork02/evenOddTable.h
@@ -0,0 +1,7 @@
+#define MIN_INT_VALUE -10
+#define MAX_INT_VALUE 10
+#define ODD_PARITY "ODD"
+#define EVEN_PARITY "EVEN"
+
+int compute_parity(int value, int *odds, int *evens);
+void print_output_line(int value, int squaredValue, int parity);
