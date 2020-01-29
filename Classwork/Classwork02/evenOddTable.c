8c792e9ae80772a6bfeff0049d6a47b316b6ab6b
Tyler Ilunga <tylerilunga1@gmail.com>
2020-01-29 11:29:16 -0800

    refactor classwork02

--- Classwork/Classwork02/evenOddTable.c
+++ Classwork/Classwork02/evenOddTable.c
@@ -13,23 +13,14 @@ int compute_parity(int value, int *odds, int *evens)
     return 1;
 }
 
-void print_output_line(int value, int squaredValue, int parity)
-{
-    char *parity_string = ODD_PARITY;
-    if (parity == 0)
-    {
-        parity_string = EVEN_PARITY;
-    }
-    printf("%d\t%d\t%s\n", value, squaredValue, parity_string);
-}
-
 int main()
 {
     int odds = 0;
     int evens = 0;
     for (int i = MIN_INT_VALUE; i <= MAX_INT_VALUE; i++)
     {
-        print_output_line(i, i * i, compute_parity(i, &odds, &evens));
+        printf("%d\t%d\t%s\n", i, i * i, ((i % 2 == 0)) ? ODD_PARITY : EVEN_PARITY);
+        compute_parity(i, &odds, &evens);
     }
     printf("#odd numbers = %d\n#even numbers = %d\n", odds, evens);
     return 0;
