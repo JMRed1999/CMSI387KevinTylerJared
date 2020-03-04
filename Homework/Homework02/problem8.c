ad264160c26a7d60c214f167892cdfaaad0ab814
JMRed1999 <44955643+JMRed1999@users.noreply.github.com>
2020-03-04 05:46:31 -0800

    Resolved hard-coded values

--- Homework/Homework02/problem8.c
+++ Homework/Homework02/problem8.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <time.h>
 #define ARR_SIZE_MODIFIER 215
+#define DESIRED_ELEMENT 4096
+#define TEN 10
 
 int count;
 typedef unsigned char BYTE;
@@ -9,10 +11,10 @@ typedef unsigned char BYTE;
 void loop()
 {
   int i;
-  BYTE arr[4096*ARR_SIZE_MODIFIER];
+  BYTE arr[DESIRED_ELEMENT*ARR_SIZE_MODIFIER];
   for (i = 0; i < sizeof(arr); i++)
   {
-    if ((i != 0) && (i % 4095 == 0))
+    if ((i != 0) && (i % (DESIRED_ELEMENT-1) == 0))
     {
       count++;
     }
@@ -22,7 +24,7 @@ void loop()
 int main(void)
 {
   int j;
-  for (j = 0; j < 10; j++)
+  for (j = 0; j < TEN; j++)
   {
     clock_t begin = clock();
     loop();
